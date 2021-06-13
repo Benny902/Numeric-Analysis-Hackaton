c806e93482e8caa0b289be107a4c787d46e489c8
Benny902 <73943596+Benny902@users.noreply.github.com>
2021-06-13 23:37:27 +0300

    Add files via upload

---
+++ q17/q17B.py
@@ -0,0 +1,120 @@
+
+
+import datetime
+from math import *
+
+
+def simpson_Method(func, a, b, epsilon):
+    print("Simpson method is going by the formula - (h/3)*(f(a)+2*sigma(from j=1 to last even)*f(X2j)+4*sigma(from j=1 to last odd)*f(X2j-1)+f(b))")
+    sol1 = 2 * epsilon
+    sol = 0
+
+    def run(func, a, b, slices):
+        slices += slices
+        h = (b - a) / slices
+        print("h = ", h)
+        print("a, b = {}, {}".format(a, b))
+        arr = []
+        for i in range(1, slices):
+            arr.append(a + (((b - a) * i) / slices))
+        arr.insert(0, a)
+        arr.append(b)
+        sol = func(a) + func(b)
+        print(f'Adding f(start) + f(end): {func(a)} + {func(b)}')
+        for i in range(1, len(arr) - 1):
+            if i % 2 != 0:
+                print(f'Iteration {i}, Last sum += 4 * (odd index value):\n{sol} += {4 * func(arr[i])}')
+                sol += 4 * func(arr[i])
+            else:
+                print(f'Iteration {i}, Last sum += 2 * (even index value):\n{sol} += {2 * func(arr[i])}')
+                sol += 2 * func(arr[i])
+            print()
+        print(f'Result = h/3 * {sol} = {sol * (h / 3)}')
+        sol = sol * (h / 3)
+        return sol
+
+    slices = 5
+    while abs(sol1 - sol) > epsilon:
+        sol = run(func, a, b, slices)
+        slices *= 2
+        sol1 = run(func, a, b, slices)
+    return sol1
+
+
+def find_intervals(a, b, N):
+    jump = (b - a)/N
+    intervals = []
+    j = a
+    for i in range(N + 1):
+        intervals.append(j)
+        j = j + jump
+    return intervals
+
+
+# trapezoidal rule
+def trapezoid(f,a,b,N):
+    yi=[]
+    h = (b-a)/N
+    xi = find_intervals(a,b,N)
+    for i in xi:
+        yi.append(f(i))
+    s = 0.0
+    for i in range(1,N):
+        if yi[i] != inf and yi[i] != -inf:
+            s = s + yi[i]
+            print("number iteration of trapezoid method:",i)
+            print("Approximation:",s)
+        else:
+            print("The Approximation of the number iteration",i,"of trapezoid method is -inf or inf")
+    s = (h/2)*(yi[0] + yi[N]) + h*s
+    return s
+
+
+def romberg(f,a,b,eps,nmax):
+# f     ... function to be integrated
+# [a,b] ... integration interval
+# eps   ... desired accuracy
+# nmax  ... maximal order of Romberg method
+    Q = [[0 for _ in range(nmax)] for __ in range(nmax)]
+    converged = 0
+    k=0
+    count=1
+    print("Romberg method is using trapezoid method -  ")
+    print("The formula of Trapezoidal is - sigma(from i=1 to N)*(h/2)*(f(Xi-h)+f(Xi))")
+    print("The formula of Romberg Method is - R(n,m)=1/(4^m-1)*(4^m*R(n,m-1)-R*(n-1,m-1))")
+    for i in range(0,nmax):
+        N = 2**i
+        Q[i][0] = trapezoid(f,a,b,N)
+        for k in range(0,i):
+            n = k + 2
+            Q[i][k+1] = 1.0/(4**(n-1)-1)*(4**(n-1)*Q[i][k] - Q[i-1][k])
+            print("number iteration of romberg method:",count)
+            print("Approximation:",Q[i][k])
+            count+=1
+        if (i > 0):
+            if (abs(Q[i][k+1] - Q[i][k]) < eps):
+               converged = 1
+               break
+    if nmax == 1:
+        return Q[i][k]
+    print("The result is -")
+    return Q[i][k + 1]
+
+
+def formated_solution(sol):
+    time = datetime.datetime.now()
+    return f'{sol}00000{time.day}{time.hour}{time.minute}'
+
+
+
+
+# q17
+f = lambda x: ((x**2)*e**((-x**2) - 5*x -3))*(3*x - 1)
+
+# Q8 -0.4, 0.4; Q17 0.5, 1; Q16 0.5, 1
+a = 0.5
+b = 1
+romberg_result = formated_solution(romberg(f, a, b, 0.0001, 10))
+print(romberg_result)
+symphson_result = formated_solution(simpson_Method(f, a, b, 0.0001))
+print(symphson_result)
---
+++ q8/q8A.py
@@ -0,0 +1,123 @@
+# f(x) = x ** 2 + 2 * x + 2
+# f(x1) * f(x2) < 0
+#ruth avivi 208981555
+#eden dahan 318641222
+from math import exp, sin
+
+
+def f_tag(f, x):
+    """
+    x = c
+    f'(x) = lim h->0 (f(c + h) - f(c))/h
+    """
+    h = 0.0000000001
+
+    return (f(x + h) - f(x))/h
+
+
+def biscetion(f, start, end, epsilon, max_iteration = 200):
+    c = start
+    i = 1
+    while abs(f(c)) > epsilon and max_iteration > 0:
+        c = (end + start)/2
+        if f(c) * f(start) > 0:
+            start = c
+        elif f(c) * f(end) > 0:
+            end = c
+        else:
+            return c
+        i += 1
+        max_iteration -= 1
+
+    if max_iteration == 0:
+        print(f'guess {c} is not an approximated root')
+
+    return c, i
+
+
+def newthon_raphson(f, start, end, epsilon, max_iteration = 200):
+    guess = (end + start) / 2
+    if max_iteration < 0:
+        max_iteration *= -1
+    i = 1
+    while abs(f(guess)) > epsilon and max_iteration >= 0:
+        guess = guess - f(guess)/f_tag(f, guess)
+        i += 1
+        max_iteration -= 1
+    if max_iteration == 0:
+        print(f'guess {guess} is not an approximated root')
+    return guess, i
+
+
+def secant(f, start, end, epsilon, max_iteration = 200):
+    '''
+
+    :param f:
+    :param start:
+    :param end:
+    :param epsilon:
+    :param max_iteration:
+    :return:
+    '''
+    guess1 = (end + start) / 2
+    guess2 = (end + start) / 3
+    s = 0.00000001
+    i = 0
+    while abs(f(guess2)) > epsilon and max_iteration > 0:
+        tmp = guess2
+        if f(guess2) - f(guess1) == 0:
+            guess1 += s
+        guess2 = guess2 - f(guess2) * ((guess2 - guess1)/ (f(guess2) - f(guess1)))
+        guess1 = tmp
+        max_iteration -= 1
+        i += 1
+
+    if max_iteration == 0:
+        print(f'guess {guess2} is not an approximated root')
+
+    return guess2, i
+
+
+def main():
+    # Q8 - Bisection, Newthon Raphson.
+    func = lambda x: sin(2*exp(-2*x)) / (x**2 + 5*x + 6)
+
+    #polinom = ((x ** 2) * e ** ((-x ** 2) + 5 * x - 3)) * (3 * x - 1)
+    # Q8 -1, 2; Q17 0, 1.5; Q16 0, 3
+    start = -1
+    end = 2
+    jump = 0.1
+    epsilon = 0.0000001
+    x_epsilon = 0.001
+    method_choices = {"1": biscetion, "2": newthon_raphson, "3": secant}
+    choice = input("Please enter method:\n1) Bisection\n2) Newthon raphson\n3) Secant")
+    while choice not in method_choices.keys():
+        choice = input("Please enter method:\n1) Bisection\n2) Newthon raphson\n3) Secant")
+    method = method_choices[choice]
+    print(f'{choice} has been selected')
+    roots = []
+    s = start
+    e = round(start + jump, 3)
+    root_exits = False
+    while e <= end:
+        print(f'interval ({s}, {e}):')
+        root, i = method(func, s, e, epsilon)
+        if abs(func(root)) <= epsilon and start <= root <= end:
+            for r in roots:
+                if abs(root-r) <= x_epsilon:
+                    root_exits = True
+                    break
+            if not root_exits:
+                roots.append(root)
+                print("------------------------------------")
+                print(f'root : {root}, iterations: {i}')
+                print("------------------------------------")
+            root_exits = False
+        s = e
+        e = round(e + jump, 3)
+        print("")
+    print(roots)
+
+
+if __name__ == "__main__":
+    main()
